ec1648b853074e060532bc1e8e03026391592dff
Euan <euan.goddard@gmail.com>
2014-08-19 20:45:22 +0100

    Improved performance of pulling new content into viewport

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
 (function ($) {
     'use strict';
 
-    var $timeline_block;
-
     $(function () {
-        $timeline_block = $('.cd-timeline-block');
         hide_timeline_blocks_outside_viewport();
-        $(window).on('scroll', show_timeline_blocks_outside_viewport);
+        $(window).on('scroll', show_timeline_blocks_inside_viewport);
     });
 
     var hide_timeline_blocks_outside_viewport = function () {
         var cut_off_top = get_cut_off_top();
-        $timeline_block.each(function () {
+        $('.cd-timeline-block').each(function () {
             var $block = $(this);
             if ($block.offset().top > cut_off_top) {
                 $block.find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
@@ -19,12 +16,14 @@
         });
     };
 
-    var show_timeline_blocks_outside_viewport = function () {
+    var show_timeline_blocks_inside_viewport = function () {
         var cut_off_top = get_cut_off_top();
-        $timeline_block.each(function () {
-            var $block = $(this);
-            if ($block.offset().top <= cut_off_top && $block.find('.cd-timeline-img').hasClass('is-hidden')) {
-                $block.find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+        $('.cd-timeline-img.is-hidden').each(function () {
+            var $img = $(this);
+            var $block = $img.parent();
+            if ($block.offset().top <= cut_off_top) {
+                var $elements = $img.next('.cd-timeline-content').addBack();
+                $elements.removeClass('is-hidden').addClass('bounce-in');
             }
         });
     };
