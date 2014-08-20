744a4ee552b034271d4ca3d85125a1d52dfef707
Euan Goddard <euangoddard@2degreesnetwork.com>
2014-08-20 13:27:33 +0100

    Added style and interaction to dot timeline

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 (function ($) {
     'use strict';
 
-    $(function () {
-        hide_timeline_blocks_outside_viewport();
-        $(window).on('scroll', show_timeline_blocks_inside_viewport);
-    });
-
     var hide_timeline_blocks_outside_viewport = function () {
         var cut_off_top = get_cut_off_top();
         $('.cd-timeline-block').each(function () {
@@ -32,5 +27,41 @@
         var cut_off_top = $(window).scrollTop() + $(window).height() * 0.75;
         return cut_off_top;
     };
+
+    var position_dots = function () {
+        var $footer = $('footer');
+        var start_date = new Date($footer.data('start-date'));
+        var end_date = new Date($footer.data('end-date'));
+        var date_span = end_date - start_date;
+        $footer.find('.dot').each(function () {
+            var $dot = $(this);
+            var date = new Date($dot.data('post-date'));
+            var delta = date - start_date;
+            var delta_percent = 100 * delta / date_span;
+            $dot.css({left: delta_percent + '%'});
+        });
+    };
+
+    var jump_to_post = function () {
+        var $dot = $(this);
+        var post_id = $(this).data('post-id');
+        var $post = $('#' + post_id);
+        if ($post.length) {
+            $('html,body').animate(
+                {scrollTop: $post.offset().top},
+                500,
+                show_timeline_blocks_inside_viewport
+            );
+        }
+    };
+
+
+    $(function () {
+        hide_timeline_blocks_outside_viewport();
+        $(window).on('scroll', show_timeline_blocks_inside_viewport);
+        
+        position_dots();
+        $(document).on('click', '.dot', jump_to_post);
+    });
 
 }(window.jQuery));
